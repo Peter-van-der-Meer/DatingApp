3bcb20cbe3ca13080f0c80e5b971bcdcd096c4e1
Peter van der Meer <PeterLovesTexel@gmail.com>
2021-11-08 21:23:35 +0100

    End of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -52,6 +52,7 @@ namespace API {
             app.UseRouting();
 
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            //app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
             app.UseAuthentication();
             app.UseAuthorization();
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { isObservable } from 'rxjs';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -10,19 +12,16 @@ import { isObservable } from 'rxjs';
 export class AppComponent implements OnInit {
   title = 'The Dating app';
   users: any;
-  dummy: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit() {
-    this.getUsers();    
-    this.dummy = "Peter";
+    this.setCurrentUser();
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      response => { this.users = response; },
-      error => { console.log(error); }
-    )
+  setCurrentUser() {
+    /* ToDo onderstaande fixen */
+    //const user: User = JSON.parse(localStorage.getItem('user'));
+    //this.accountService.setCurrentUser(user);
   }
 }
